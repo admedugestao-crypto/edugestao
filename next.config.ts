eb05245d3ea376aae79acf6de8982ac3bc4b0ea2
edugestao <adm.edugestao@gmail.com>
2026-05-16 16:48:16 -0300

    adiciona serverExternalPackages para prisma

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  serverExternalPackages: ["@prisma/client", "@/generated/prisma/client"],
 };
 
 export default nextConfig;
