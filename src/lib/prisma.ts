e31ff9c2ab6054d95449c25e497d18851b68a73d
edugestao <adm.edugestao@gmail.com>
2026-05-16 16:20:53 -0300

    corrige import e instancia prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from "../../src/generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL as string,
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
